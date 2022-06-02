4073ff73731def707f779da099964fd4b854cde0
Arnold-Js <r.a.p23cruzortiz@gmail.com>
2022-06-02 06:12:53 -0500

    incorporacion del proyecto visualizacion de datos

---
+++ Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace BD_Proyecto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.noControl = new System.Windows.Forms.Label();
+            this.nombre = new System.Windows.Forms.Label();
+            this.apellidoPaterno = new System.Windows.Forms.Label();
+            this.apellidoMaterno = new System.Windows.Forms.Label();
+            this.semestre = new System.Windows.Forms.Label();
+            this.correo = new System.Windows.Forms.Label();
+            this.txtBoxnoControl = new System.Windows.Forms.TextBox();
+            this.txtBoxnombre = new System.Windows.Forms.TextBox();
+            this.txtBoxapellidoPaterno = new System.Windows.Forms.TextBox();
+            this.txtBoxapellidoMaterno = new System.Windows.Forms.TextBox();
+            this.txtBoxsemestre = new System.Windows.Forms.TextBox();
+            this.txtBoxcorreo = new System.Windows.Forms.TextBox();
+            this.BotonBuscar = new System.Windows.Forms.Button();
+            this.BotonCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // noControl
+            // 
+            this.noControl.AutoSize = true;
+            this.noControl.Location = new System.Drawing.Point(35, 34);
+            this.noControl.Name = "noControl";
+            this.noControl.Size = new System.Drawing.Size(54, 13);
+            this.noControl.TabIndex = 0;
+            this.noControl.Text = "NoControl";
+            // 
+            // nombre
+            // 
+            this.nombre.AutoSize = true;
+            this.nombre.Location = new System.Drawing.Point(35, 56);
+            this.nombre.Name = "nombre";
+            this.nombre.Size = new System.Drawing.Size(44, 13);
+            this.nombre.TabIndex = 1;
+            this.nombre.Text = "Nombre";
+            // 
+            // apellidoPaterno
+            // 
+            this.apellidoPaterno.AutoSize = true;
+            this.apellidoPaterno.Location = new System.Drawing.Point(35, 82);
+            this.apellidoPaterno.Name = "apellidoPaterno";
+            this.apellidoPaterno.Size = new System.Drawing.Size(84, 13);
+            this.apellidoPaterno.TabIndex = 2;
+            this.apellidoPaterno.Text = "Apellido Paterno";
+            // 
+            // apellidoMaterno
+            // 
+            this.apellidoMaterno.AutoSize = true;
+            this.apellidoMaterno.Location = new System.Drawing.Point(35, 108);
+            this.apellidoMaterno.Name = "apellidoMaterno";
+            this.apellidoMaterno.Size = new System.Drawing.Size(86, 13);
+            this.apellidoMaterno.TabIndex = 3;
+            this.apellidoMaterno.Text = "Apellido Materno";
+            // 
+            // semestre
+            // 
+            this.semestre.AutoSize = true;
+            this.semestre.Location = new System.Drawing.Point(38, 135);
+            this.semestre.Name = "semestre";
+            this.semestre.Size = new System.Drawing.Size(51, 13);
+            this.semestre.TabIndex = 4;
+            this.semestre.Text = "Semestre";
+            // 
+            // correo
+            // 
+            this.correo.AutoSize = true;
+            this.correo.Location = new System.Drawing.Point(41, 161);
+            this.correo.Name = "correo";
+            this.correo.Size = new System.Drawing.Size(38, 13);
+            this.correo.TabIndex = 5;
+            this.correo.Text = "Correo";
+            // 
+            // txtBoxnoControl
+            // 
+            this.txtBoxnoControl.Location = new System.Drawing.Point(139, 27);
+            this.txtBoxnoControl.Name = "txtBoxnoControl";
+            this.txtBoxnoControl.Size = new System.Drawing.Size(179, 20);
+            this.txtBoxnoControl.TabIndex = 6;
+            // 
+            // txtBoxnombre
+            // 
+            this.txtBoxnombre.Location = new System.Drawing.Point(139, 53);
+            this.txtBoxnombre.Name = "txtBoxnombre";
+            this.txtBoxnombre.Size = new System.Drawing.Size(179, 20);
+            this.txtBoxnombre.TabIndex = 7;
+            // 
+            // txtBoxapellidoPaterno
+            // 
+            this.txtBoxapellidoPaterno.Location = new System.Drawing.Point(139, 79);
+            this.txtBoxapellidoPaterno.Name = "txtBoxapellidoPaterno";
+            this.txtBoxapellidoPaterno.Size = new System.Drawing.Size(179, 20);
+            this.txtBoxapellidoPaterno.TabIndex = 8;
+            // 
+            // txtBoxapellidoMaterno
+            // 
+            this.txtBoxapellidoMaterno.Location = new System.Drawing.Point(139, 105);
+            this.txtBoxapellidoMaterno.Name = "txtBoxapellidoMaterno";
+            this.txtBoxapellidoMaterno.Size = new System.Drawing.Size(179, 20);
+            this.txtBoxapellidoMaterno.TabIndex = 9;
+            // 
+            // txtBoxsemestre
+            // 
+            this.txtBoxsemestre.Location = new System.Drawing.Point(139, 132);
+            this.txtBoxsemestre.Name = "txtBoxsemestre";
+            this.txtBoxsemestre.Size = new System.Drawing.Size(179, 20);
+            this.txtBoxsemestre.TabIndex = 10;
+            // 
+            // txtBoxcorreo
+            // 
+            this.txtBoxcorreo.Location = new System.Drawing.Point(139, 158);
+            this.txtBoxcorreo.Name = "txtBoxcorreo";
+            this.txtBoxcorreo.Size = new System.Drawing.Size(179, 20);
+            this.txtBoxcorreo.TabIndex = 11;
+            // 
+            // BotonBuscar
+            // 
+            this.BotonBuscar.Location = new System.Drawing.Point(379, 21);
+            this.BotonBuscar.Name = "BotonBuscar";
+            this.BotonBuscar.Size = new System.Drawing.Size(77, 26);
+            this.BotonBuscar.TabIndex = 12;
+            this.BotonBuscar.Text = "Buscar";
+            this.BotonBuscar.UseVisualStyleBackColor = true;
+            this.BotonBuscar.Click += new System.EventHandler(this.BotonBuscar_Click);
+            // 
+            // BotonCerrar
+            // 
+            this.BotonCerrar.Location = new System.Drawing.Point(38, 213);
+            this.BotonCerrar.Name = "BotonCerrar";
+            this.BotonCerrar.Size = new System.Drawing.Size(77, 26);
+            this.BotonCerrar.TabIndex = 13;
+            this.BotonCerrar.Text = "Cerrar";
+            this.BotonCerrar.UseVisualStyleBackColor = true;
+            this.BotonCerrar.Click += new System.EventHandler(this.BotonCerrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 262);
+            this.Controls.Add(this.BotonCerrar);
+            this.Controls.Add(this.BotonBuscar);
+            this.Controls.Add(this.txtBoxcorreo);
+            this.Controls.Add(this.txtBoxsemestre);
+            this.Controls.Add(this.txtBoxapellidoMaterno);
+            this.Controls.Add(this.txtBoxapellidoPaterno);
+            this.Controls.Add(this.txtBoxnombre);
+            this.Controls.Add(this.txtBoxnoControl);
+            this.Controls.Add(this.correo);
+            this.Controls.Add(this.semestre);
+            this.Controls.Add(this.apellidoMaterno);
+            this.Controls.Add(this.apellidoPaterno);
+            this.Controls.Add(this.nombre);
+            this.Controls.Add(this.noControl);
+            this.Name = "Form1";
+            this.Text = "estudiante";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label noControl;
+        private System.Windows.Forms.Label nombre;
+        private System.Windows.Forms.Label apellidoPaterno;
+        private System.Windows.Forms.Label apellidoMaterno;
+        private System.Windows.Forms.Label semestre;
+        private System.Windows.Forms.Label correo;
+        private System.Windows.Forms.TextBox txtBoxnoControl;
+        private System.Windows.Forms.TextBox txtBoxnombre;
+        private System.Windows.Forms.TextBox txtBoxapellidoPaterno;
+        private System.Windows.Forms.TextBox txtBoxapellidoMaterno;
+        private System.Windows.Forms.TextBox txtBoxsemestre;
+        private System.Windows.Forms.TextBox txtBoxcorreo;
+        private System.Windows.Forms.Button BotonBuscar;
+        private System.Windows.Forms.Button BotonCerrar;
+    }
+}
+
